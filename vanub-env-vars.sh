d1c24cef994e73027b1da627104e01e6cdaab3d4
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-06-24 11:37:01 +0100

    Update vanub-env-vars.sh

--- vanub-env-vars.sh
+++ vanub-env-vars.sh
@@ -1,6 +1,6 @@
 # change this value to the latest ubuntu version details for an upgrade
-export VANUBUNTU_VERSION_CODE = "lunar"
-export VANUBUNTU_VERSION = "23.04"
+export VANUBUNTU_VERSION_CODE="lunar"
+export VANUBUNTU_VERSION="23.04"
 
 # is this version in development?
 # 0: no
@@ -8,4 +8,4 @@ export VANUBUNTU_VERSION = "23.04"
 #
 # ALWAYS SET 1 IF THE ORIGINAL UBUNTU VERSION HAS STILL NOT ENTERED
 # STABLE!
-export VANUBUNTU_VERSION_DEV = 1
+export VANUBUNTU_VERSION_DEV=1
