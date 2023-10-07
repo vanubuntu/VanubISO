d02de30b1432bb0d8aa63fcc884da9d1bed0c048
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-10-07 20:54:15 +0100

    Create vanubuntu-flatpak.sh

---
+++ topping/vanubuntu-flatpak.sh
@@ -0,0 +1,5 @@
+echo "Vanubuntu Flatpak"
+echo "Installing Flatpak..."
+schroot -c vanubuntu -- apt-get install -y gnome-core gnome-boxes gnome-connections gnome-core
+echo "Added a delicious topping for the best software."
+echo "Tasty! :D"
