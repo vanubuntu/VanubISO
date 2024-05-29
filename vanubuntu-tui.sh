fdfcb075e278305dfa02e1342c2016d21e765632
rickbot dot <115214762+rickbot-dot@users.noreply.github.com>
2024-05-29 13:35:30 +0100

    Create vanubuntu-tui.sh

---
+++ vanubuntu-tui.sh
@@ -0,0 +1,14 @@
+function menu {
+whiptail \
+  --notags --backtitle "VanubISO TUI" \
+  --menu "UP and DOWN to select a menu. Select OK to enter the selected menu." \
+  25 75 5 \
+  editfile "Edit APT files" \
+  editscript "Edit build script (not recommended)" \
+  toppings "Select Packages" \
+  basicbuild "Perform Basic Build"
+}
+
+while true; do
+  OPTION=menu
+done
