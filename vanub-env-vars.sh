66530366936842737ad6a664f0a45217987f6fcc
rickbot dot <115214762+rickbot-dot@users.noreply.github.com>
2024-05-29 11:03:52 +0100

    Update vanub-env-vars.sh

--- vanub-env-vars.sh
+++ vanub-env-vars.sh
@@ -1,9 +1,7 @@
 # change this value to the latest ubuntu version details for an upgrade
-# due to compatibility reasons we are using jammy script
-# when 24.04 comes out we will use it
 # if we can move to a custom runner we can follow the latest update cycle
 export VANUBUNTU_VERSION_CODE="jammy"
-export VANUBUNTU_VERSION="22.04"
+export VANUBUNTU_VERSION="24.04"
 
 # is this version in development?
 # 0: no
