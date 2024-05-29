bc54dee0fd5fb556528999043cd84aed28191433
rickbot dot <115214762+rickbot-dot@users.noreply.github.com>
2024-05-29 11:01:07 +0100

    Update make-prefs.sh

--- make-prefs.sh
+++ make-prefs.sh
@@ -11,6 +11,8 @@ cat <<EOF | sudo tee /etc/apt/preferences.d/no-ud.pref
 # Due to ubuntu's sneaky installer scripting, this apt configuration will
 # prevent ubuntu-desktop from being installed. ubuntu-minimal can still
 # be installed though.
+# This prevents Vanubuntu configuration from being overitten by the hacky
+# Ubuntu files.
 Package: ubuntu-desktop
 Pin: release a=*
 Pin-Priority: -10
